ed5a405b3bcd4c045ce54905352c7c5a03905bec
Wayne J. Ryan <wryan@smithdrug.com>
2022-01-30 10:46:58 -0500

    Adding basic backend methods

--- Project1/src/com/wryan/MainClass.java
+++ Project1/src/com/wryan/MainClass.java
@@ -1,23 +1,62 @@
 package com.wryan;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class MainClass {
+    
     public static void main(String[] args) {
-        doFileListing("C:\\junk");
+        System.out.printf(filterFileListing("[junk]", getFileListing("C:\\junk")).toString());
+
     }
 
-    public static void doFileListing(String filePath){
-        File folder = new File(filePath);
-        File[] listOfFiles = folder.listFiles();
+    public static void deleteFile(String filePath){
+        File myObj = new File(filePath);
+        if (myObj.delete()) {
+            System.out.println("Deleted the file: " + myObj.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
+    }
 
-        for (File f : listOfFiles) {
-            if (f.isFile()) {
-                System.out.println("File " + f.getName());
-            } else if (f.isDirectory()) {
-                System.out.println("Directory " + f.getName());
-                doFileListing(filePath + "\\" + f.getName());
+    public static ArrayList<String> filterFileListing(String searchString, ArrayList<String> fileNames){
+        Pattern pattern = Pattern.compile(searchString, Pattern.CASE_INSENSITIVE);
+        ArrayList<String> matchingFileNames = new ArrayList<String>();
+        for (String fileName : fileNames) {
+            if(pattern.matcher(fileName).find()) {
+                matchingFileNames.add(fileName);
             }
         }
+        return matchingFileNames;
+    }
+
+    public static void createFile(String filePath){
+        try {
+            File myObj = new File(filePath);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<String> getFileListing(String filePath){
+        File folder = new File(filePath);
+        File[] topLevelFiles = folder.listFiles();
+        ArrayList<String> fileNames = new ArrayList<String>();
+        for (File f : topLevelFiles) {
+            if (f.isFile()) {
+                fileNames.add(f.getName());
+            } else if (f.isDirectory()) {
+                fileNames.addAll(getFileListing(filePath + "\\" + f.getName()));
+            }
+        }
+        return fileNames;
     }
 }
