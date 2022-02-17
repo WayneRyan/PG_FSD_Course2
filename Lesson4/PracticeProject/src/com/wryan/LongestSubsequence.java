b4d633abf1b396cba11dbd0774f0b241c420fd27
Wayne J. Ryan <wryan@smithdrug.com>
2022-02-17 07:56:22 -0500

    Generalized the computation added some defensive programming

--- Lesson4/PracticeProject/src/com/wryan/LongestSubsequence.java
+++ Lesson4/PracticeProject/src/com/wryan/LongestSubsequence.java
@@ -1,26 +1,88 @@
 package com.wryan;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class LongestSubsequence {
-    public static void main(String[] args) {
-        int[] sequence = {10,22,9,33,21,50,41,60,80};
-        int[] memoization = {1,1,1,1,1,1,1,1,1};
+
+    private int[] sequence = new int[0];
+    private int[] memoization = new int[0];
+    private int[] links = new int[0];
+    private int maxVal = 1;
+    private int maxIndex = 0;
+
+    private LongestSubsequence(int sequenceLength, int maxValue) {
+        sequence = new int[sequenceLength];
+        memoization = new int[sequenceLength];
+        links = new int[sequenceLength];
+        for (int i = 0; i < sequenceLength; i++) {
+            sequence[i] = (int) (Math.random() * maxValue);
+            memoization[i] = 1;
+            links[i] = i;
+        }
+        System.out.println("The random sequence is:");
         System.out.println(Arrays.toString(sequence));
-        int result = 1;
-        for (int i=1 ; i<sequence.length ; i++){
-            for (int j=i-1 ; j>=0 ; j--){
-                if (sequence[j] < sequence[i] && memoization[i]<=memoization[j]){
-                    int updateVal = memoization[j]+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This program computes a longest increasing sequence contained in a random sequence.");
+        System.out.println("How long do you want the random sequence to be?");
+        int sequenceLength = fetchPositiveInt();
+        System.out.println("The sequence will contain random integers from zero to some maximum value.");
+        System.out.println("What is the maximum value the sequence should contain?");
+        int sequenceMaxValue =  fetchPositiveInt();
+        LongestSubsequence ls = new LongestSubsequence(sequenceLength, sequenceMaxValue);
+        ls.calculate();
+        ls.printLongestSubsequence();
+    }
+
+    public static int fetchPositiveInt(){
+        int inputValue = -1;
+        try {
+            Scanner in = new Scanner(System.in);
+            while (inputValue<=0){
+                inputValue =  in.nextInt();
+                if (inputValue<=0) {
+                    System.out.println("Please chose a POSITIVE integer");
+                }
+            }
+        } catch (Exception e){
+            System.out.println("Something went wrong. Please try again:");
+            return fetchPositiveInt();
+        }
+        return inputValue;
+    }
+
+
+    private void calculate() {
+        for (int i = 1; i < sequence.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (sequence[j] < sequence[i] && memoization[i] <= memoization[j]) {
+                    int updateVal = memoization[j] + 1;
                     memoization[i] = updateVal;
-                    if(result<updateVal){
-                        result = updateVal;
+                    links[i] = j;
+                    if (maxVal < updateVal) {
+                        maxVal = updateVal;
+                        maxIndex = i;
                         break;
                     }
                 }
             }
         }
-        System.out.println(Arrays.toString(memoization));
     }
+
+    private void printLongestSubsequence() {
+        printLongestSubsequence(maxIndex);
+    }
+
+    private void printLongestSubsequence(int index) {
+        if (links[index] != index) {
+            printLongestSubsequence(links[index]);
+        } else {
+            System.out.println("The longest sequence has " + maxVal + " elements.");
+            System.out.println("The following is a longest increasing sequence in that random sequence.");
+        }
+        System.out.println(sequence[index]);
+    }
+
 }
